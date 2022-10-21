fb7774b07d26eab6d4475fa364ae2083fd08f0e7
xiyuzhou <32187988+xiyuzhou@users.noreply.github.com>
2022-10-21 12:10:52 -0700

    Add files via upload

---
+++ Projectfiles/Assets/Client.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class Client : MonoBehaviour
+{
+    public bool IhasLeg;
+    public bool ICanSwim;
+    public bool IEdible;
+
+    public Toggle Leg;
+    public Toggle Swim;
+    public Toggle Edi;
+    public Text text1;
+
+    public void OnPress()
+    {
+        AnimalRequirement requirements = new AnimalRequirement();
+        requirements.hasLeg = Leg.isOn;
+        requirements.CanSwim = Swim.isOn;
+        requirements.Edible = Edi.isOn;
+        IAnimal k = GetAnimal(requirements);
+        text1.text = k.ToString();
+        Debug.Log(k);
+    }
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+
+    private static IAnimal GetAnimal(AnimalRequirement requirements)
+    {
+        AnimalFactory factory = new AnimalFactory(requirements);
+        return factory.Create();
+    }
+}
